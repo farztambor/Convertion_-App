d175552c135e03172f2f74a9fe9b9fc14974b88d
farztambor <raffie.lanticse@gmail.com>
2022-05-21 16:00:01 +0800

    Done Input User

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let feetEl = document.getElementById("feet-el")
-let inputEl = document.getElementById("input-el")
+let inputEl = document.getElementById("input-el").value
 let gallonEl = document.getElementById("gallons-el")
 let poundsEl = document.getElementById("pounds-el")
-let inputVal = 20
+let convertBtn = document.getElementById("convert-btn")
+let inputVal = inputEl
 
 
-inputEl.textContent = inputVal
+convertBtn.addEventListener("click", function(){
+    inputEl = document.getElementById("input-el").value
+    inputVal = inputEl
+    convert()
+})
 
-feetEl.textContent = inputVal+ " meters = "  + (inputVal*3.28084).toFixed(3) + " feet " + "| " + inputVal+ " feet " + (inputVal*0.3048) + " = meters" 
-gallonEl.textContent =  inputVal + " liters = "  + (inputVal*0.264172).toFixed(3) + " gallons " + "| " + inputVal+" gallons " + (inputVal*3.78541).toFixed(3) + " liters"
-poundsEl.textContent =  inputVal + " pounds = "  + (inputVal*0.453592).toFixed(3) + " kilos " + "| " + inputVal+" kilos " + (inputVal*2.20462).toFixed(3) + " pounds"
+function convert(){
+    inputEl.textContent = inputVal
+    feetEl.textContent = inputVal+ " meters = "  + (inputVal*3.28084).toFixed(3) + " feet " + "| " + inputVal+ " feet " + (inputVal*0.3048).toFixed(3) + " = meters" 
+    gallonEl.textContent =  inputVal + " liters = "  + (inputVal*0.264172).toFixed(3) + " gallons " + "| " + inputVal+" gallons " + (inputVal*3.78541).toFixed(3) + " liters"
+    poundsEl.textContent =  inputVal + " pounds = "  + (inputVal*0.453592).toFixed(3) + " kilos " + "| " + inputVal+" kilos " + (inputVal*2.20462).toFixed(3) + " pounds"
+}
+    convert()
